9ea2919a61313da3bcab41a5720b50e967ea041e
vas59 <vasily.yuryev.a1@gmail.com>
2019-08-04 15:35:30 +0300

    first commit

---
+++ tcp_vpe_sender.cpp
@@ -0,0 +1,114 @@
+// #include "src/helpers.h"
+#include "drone.h"
+#include "tcp.h"
+#include "udp.h"
+#include <opencv2/opencv.hpp>
+#include <opencv2/ximgproc.hpp>
+#include <opencv2/aruco.hpp>
+
+#include "markers_lib/src/markers.h"
+#include "markers_lib/src/aruco_markers.h"
+#include "markers_lib/src/solver.h"
+
+using namespace markers_lib;
+using namespace mavlink_indoor_sdk;
+
+ArucoMarkersDetector aruco_detector;
+Solver solver;
+
+// Config
+string calibration_file = "./log.yml";
+string map_url = "./map.txt";
+string map_jpeg = "./map.jpg";
+int map_jpeg_size = 1000;
+int dictinary = 3;
+int cam_id = 2;
+
+struct VisionData
+{
+    Pose pose;
+    bool success;
+};
+
+VisionData process_img(Mat img)
+{
+    Mat viz;
+    img.copyTo(viz);
+    VisionData vd;
+    Mat objPoints, imgPoints;
+    aruco_detector.detect(img, objPoints, imgPoints, viz);
+    // aruco_detector.drawViz(viz);
+
+    // cout << objPoints << "img: " << imgPoints << "\n";
+    // Pose pose;
+    vd.success = solver.solve(objPoints, imgPoints, vd.pose, viz);
+    imshow("Viz", viz);
+    return vd;
+}
+
+void load_config()
+{
+    FileStorage fs2("config.yml", FileStorage::READ);
+    fs2["map"] >> map_url;
+    fs2["calibration_file"] >> calibration_file;
+
+    // fs2["connection_url"] >> connection_url;
+
+    fs2["map_jpeg"] >> map_jpeg;
+    fs2["map_jpeg_size"] >> map_jpeg_size;
+    fs2["dictinary"] >> dictinary;
+    fs2["cam_id"] >> cam_id;
+    fs2.release();
+}
+
+void init_marker_reg()
+{
+
+    solver.load_camera_conf(calibration_file);
+
+    // solver.set_camera_conf(cameraMatrix, distCoeffs);
+
+    aruco_detector.setDictionary(cv::aruco::getPredefinedDictionary(dictinary));
+
+    aruco_detector.loadMap(map_url);
+    aruco_detector.genBoard();
+    Mat map_img =
+        aruco_detector.drawBoard(cv::Size(map_jpeg_size, map_jpeg_size));
+    imwrite(map_jpeg, map_img);
+}
+
+int main()
+{
+    load_config();
+    init_marker_reg();
+
+    TCP_Protocol lw_proto("tcp://127.0.0.1:5761");
+    lw_proto.start();
+
+    VideoCapture cap(cam_id);
+    while (true)
+    {
+        if (lw_proto.status == 1)
+        {
+            mavlink_message_t msg;
+            mavlink_vision_position_estimate_t vpe;
+            Mat frame;
+            cap >> frame;
+            VisionData vd = process_img(frame);
+
+            vpe.usec = get_time_usec();
+            vpe.x = vd.pose.pose.x;
+            vpe.y = vd.pose.pose.y;
+            vpe.z = vd.pose.pose.z;
+
+            vpe.roll = vd.pose.rotation.y;
+            vpe.pitch = vd.pose.rotation.x;
+            vpe.yaw = vd.pose.rotation.z;
+
+            mavlink_msg_vision_position_estimate_encode(12, MAV_COMP_ID_ALL, &msg, &vpe);
+            lw_proto.write_message(msg);
+            usleep(10000);
+        }
+    }
+    lw_proto.stop();
+}
---
+++ udp_telemetry_test.cpp
@@ -0,0 +1,45 @@
+// #include "src/helpers.h"
+#include "drone.h"
+#include "tcp.h"
+#include "udp.h"
+
+using namespace mavlink_indoor_sdk;
+
+int main(){
+    UDP_Protocol lw_proto("udp://127.0.0.1:14520");
+    lw_proto.start();
+    
+    autopilot_interface::AutopilotInterface ai(&lw_proto);
+
+    Drone drone(&ai);
+    drone.start();
+
+    // ai.system_id = 1;
+    drone.arm();
+    // drone.takeoff(1.5, 0.5);
+    drone.set_position({0, 0, 2, 0}, FRAME_LOCAL);
+    drone.sleep(3000);
+
+    drone.navigate_wait({2, 0, 2, 0}, FRAME_LOCAL, 1.5);
+    
+    drone.sleep(3000);
+
+    drone.navigate_wait({-2, 0, 2, 0}, FRAME_LOCAL, 0.5);
+    
+    drone.sleep(3000);
+
+    drone.navigate_wait({0, 0, 2, 1.57}, FRAME_LOCAL, 0.5);
+    // drone.set_position({0, 0, 2, 1.57}, FRAME_LOCAL);
+    drone.sleep(3000);
+    // drone.arm();
+    // drone.takeoff(1.5, 0.5);
+
+    // drone.navigate({0, 0, 2, 0}, FRAME_VISION, 0.5);
+
+    drone.land();
+    // drone.sleep(5000);
+    // drone.disarm();
+
+    drone.stop();
+    lw_proto.stop();
+}
