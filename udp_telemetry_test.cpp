04b2622c1a59a3d1a4c5547db684f67812d28990
vas59 <vasily.yuryev.a1@gmail.com>
2019-08-09 21:23:45 +0300

    test

--- udp_telemetry_test.cpp
+++ udp_telemetry_test.cpp
@@ -22,11 +22,11 @@ int main(){
     // drone.sleep(50*60000);
     drone.sleep(1000);
     // drone.set_position({0, 1, 2, 0}, FRAME_VISION);
-    drone.navigate({0, 0, 2, 0}, FRAME_LOCAL, 0.5);
-    drone.navigate({2, 2, 2, 0}, FRAME_LOCAL, 0.5);
-    drone.sleep(10000);
-    // drone.set_position({0, 1, 2, 0}, FRAME_VISION);
-    drone.navigate({-2, -2, 2, 0}, FRAME_LOCAL, 0.5);
+    drone.navigate_wait({0, 0, 2, 0}, FRAME_LOCAL, 0.5);
+    drone.navigate_wait({2, 2, 2, 0}, FRAME_LOCAL, 0.5);
+    // drone.sleep(10000);
+    // // drone.set_position({0, 1, 2, 0}, FRAME_VISION);
+    // drone.navigate({-2, -2, 2, 0}, FRAME_LOCAL, 0.5);
     while (true){
         cout << "Local " << drone.get_telemetry(FRAME_LOCAL).ToString();
         cout << "Vision " << drone.get_telemetry(FRAME_VISION).ToString();
