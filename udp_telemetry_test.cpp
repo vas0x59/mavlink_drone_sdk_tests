1c6c9d81bdd621a1025aec1632a3657872e977db
vas59 <vasily.yuryev.a1@gmail.com>
2019-08-09 20:51:55 +0300

    tests

--- udp_telemetry_test.cpp
+++ udp_telemetry_test.cpp
@@ -6,7 +6,7 @@
 using namespace mavlink_indoor_sdk;
 
 int main(){
-    UDP_Protocol lw_proto("udp://127.0.0.1:14520");
+    UDP_Protocol lw_proto("udp://127.0.0.1:14530");
     lw_proto.start();
     
     autopilot_interface::AutopilotInterface ai(&lw_proto);
@@ -17,20 +17,33 @@ int main(){
     // ai.system_id = 1;
     drone.arm();
     // drone.takeoff(1.5, 0.5);
-    drone.set_position({0, 0, 2, 0}, FRAME_LOCAL);
-    drone.sleep(3000);
+    drone.set_position({0, 0, 4, 0}, FRAME_LOCAL);
+    // drone.set_position({0, 1, 2, 0}, FRAME_VISION);
+    // drone.sleep(50*60000);
+    drone.sleep(1000);
+    // drone.set_position({0, 1, 2, 0}, FRAME_VISION);
+    drone.navigate({0, 0, 2, 0}, FRAME_LOCAL, 0.5);
+    drone.navigate({2, 2, 2, 0}, FRAME_LOCAL, 0.5);
+    drone.sleep(10000);
+    // drone.set_position({0, 1, 2, 0}, FRAME_VISION);
+    drone.navigate({-2, -2, 2, 0}, FRAME_LOCAL, 0.5);
+    while (true){
+        cout << "Local " << drone.get_telemetry(FRAME_LOCAL).ToString();
+        cout << "Vision " << drone.get_telemetry(FRAME_VISION).ToString();
+        drone.sleep(333);
+    }
 
-    drone.navigate_wait({2, 0, 2, 0}, FRAME_LOCAL, 1.5);
+    // drone.navigate_wait({0, 0, 2, 0}, FRAME_LOCAL, 1.5);
     
-    drone.sleep(3000);
+    // drone.sleep(3000);
 
-    drone.navigate_wait({-2, 0, 2, 0}, FRAME_LOCAL, 0.5);
+    // drone.navigate_wait({-2, 0, 2, 0}, FRAME_LOCAL, 0.5);
     
-    drone.sleep(3000);
+    // drone.sleep(3000);
 
-    drone.navigate_wait({0, 0, 2, 1.57}, FRAME_LOCAL, 0.5);
+    // drone.navigate_wait({0, 0, 2, 1.57}, FRAME_LOCAL, 0.5);
     // drone.set_position({0, 0, 2, 1.57}, FRAME_LOCAL);
-    drone.sleep(3000);
+    // drone.sleep(3000);
     // drone.arm();
     // drone.takeoff(1.5, 0.5);
 
