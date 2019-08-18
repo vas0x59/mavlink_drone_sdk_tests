8a4fe9e09c6bd699c673c214c0d06ee6e2f56548
vas59 <vasily.yuryev.a1@gmail.com>
2019-08-18 12:21:33 +0300

    build script

---
+++ build_all.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+for var in mavlink_indoor_sdk markers_lib led_uart_cpp
+do
+    echo "./$var"
+    (cd "./$var" && ./build.sh $1 $2)
+    
+done
+
+# if ["$arch" == "arm"] ; 
+# then
+#     echo "arm"
+#     if "$rebuild"; then
+#         rm -rf build_arm
+#         mkdir build_arm
+#     fi
+#     cd build_arm
+#     cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_TOOLCHAIN_FILE=$HOME/rpi/toolchain_rpi.cmake ..
+#     make 
+# else
+#     echo "this"
+#     if "$rebuild"; then
+#         rm -rf build
+#         mkdir build
+#     fi
+#     cd build
+#     cmake -D CMAKE_BUILD_TYPE=RELEASE ..
+#     make
+# fi
