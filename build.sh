28eefdc096a074a84de25d614125ba41bb3d53ea
vas59 <vasily.yuryev.a1@gmail.com>
2019-08-20 09:33:10 +0300

    build

--- build.sh
+++ build.sh
@@ -7,24 +7,18 @@ then
     then
         rm -rf build_arm
         mkdir build_arm
-        cd build_arm
-        cmake -D DEP2_BUILD_DIR=build_arm -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_TOOLCHAIN_FILE=$HOME/rpi/toolchain_rpi.cmake ..
-    else
-        cd build_arm
+        (cd build_arm && cmake -D DEP2_BUILD_DIR=build_arm -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_TOOLCHAIN_FILE=$HOME/rpi/toolchain_rpi.cmake .. )
     fi
-
-    make $3
+    (cd build_arm && make)
+    
+    (cd build_arm && cmake -D DEP2_BUILD_DIR=build_arm -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_TOOLCHAIN_FILE=$HOME/rpi/toolchain_rpi.cmake ..)
 else
     echo $(arch)
     if  [ "$2" = "1" ]
     then
         rm -rf build
         mkdir build
-        cd build
-        cmake -D DEP2_BUILD_DIR=build -D CMAKE_BUILD_TYPE=RELEASE ..
-    else
-        cd build
+        (cd build && cmake -D DEP2_BUILD_DIR=build -D CMAKE_BUILD_TYPE=RELEASE ..)
     fi
-    
-    make $3
+    (cd build && make)
 fi
