22fe78001b3274be28474e7d6c8f9bf335ada00d
vas59 <vasily.yuryev.a1@gmail.com>
2019-08-20 13:12:36 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 host="raspberrypi.local"
+
 # read -sp 'password: ' passvar
-passvar=$(cat raspi_pass.txt)
 echo ""
+
+passvar=$(cat raspi_pass.txt)
+
 lib_to_deploy=$(cat deploy_conf/lib_to_deploy.txt)
 bin_to_deploy=$(cat deploy_conf/bin_to_deploy.txt)
 scripts_to_deploy=$(cat deploy_conf/scripts_to_deploy.txt)
+
 echo "lib: $lib_to_deploy"
 echo "bin: $bin_to_deploy"
 echo "script: $scripts_to_deploy"
@@ -17,7 +21,8 @@ do
     scripts_to_deploy="$scripts_to_deploy ./drone_scripts/$var"
     # sshpass -p "$passvar" scp "drone_scripts/$var" pi@$host:
 done
-to_deploy="$lib_to_deploy $bin_to_deploy $scripts_to_deploy"
+
+# to_deploy="$lib_to_deploy $bin_to_deploy $scripts_to_deploy"
 
 sshpass -p "$passvar" scp $lib_to_deploy pi@$host:lib
 sshpass -p "$passvar" scp $bin_to_deploy pi@$host:bin
