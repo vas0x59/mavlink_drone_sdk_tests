15e7ea1f9a18312235084c3c4adb523538676a48
vas59 <vasily.yuryev.a1@gmail.com>
2019-08-22 17:29:33 +0700

    circle

--- src/udp_telemetry_test.cpp
+++ src/udp_telemetry_test.cpp
@@ -22,18 +22,19 @@ void usage()
          << "\n";
 }
 
-void circle(float radius, PointXYZ centre, PointXYZ start, Frame frame, float part = 2 * M_PI, int res = 30)
+void circle(float radius, PointXYZ centre, PointXYZ start, Frame frame, float part = 2 * M_PI, int res = 10)
 {
 
-    for (float i = 0; i < res; i++)
+    for (float i = 0; i <= res; i++)
     {
         float p = (i / res) * part;
         // float
         PointXYZ now = centre + PointXYZ(cosf(p) * radius, sinf(p) * radius, 0);
         LogInfo("CIRCLE", now.ToString());
-        drone->navigate_wait({now.x, now.y, now.z, 0}, frame, 2);
+        drone->navigate_wait({now.x, now.y, now.z, 0}, frame, 0.5);
         // drone->sleep(500);
     }
+    LogInfo("CIRCLE", "Done");
 }
 
 int main(int argc, char **argv)
@@ -64,32 +65,41 @@ int main(int argc, char **argv)
     drone->start();
     // ai.system_id = 1;
     drone->arm();
-    // drone->takeoff(1.5, 0.5);
-    drone->set_position({0, 0, 2, 0}, FRAME_LOCAL);
+    drone->takeoff(1.5, 0.5);
+    // drone->set_position({0, 0, 2, 0}, FRAME_LOCAL);
     drone->sleep(3000);
-    // drone->navigate_wait({0, 0, 2, 0}, FRAME_LOCAL, 0.5);
+    // drone->navigate_wait({0, 0.5, 250, 0}, FRAME_LOCAL, 5, 0.5);
     // drone->sleep(3000);
-    circle(100, {0, 0, 2}, {0, 0, 2}, FRAME_LOCAL);
+    // drone->navigate_wait({0.5, 0.5, 1.5, 0}, FRAME_LOCAL, 0.3, 0.05);
+    // drone->sleep(3000);
+    // drone->disarm();
+    // drone->sleep(1000);
+    // drone->arm();
+    // drone->set_position({0, 0, 1, 0}, FRAME_LOCAL);
+    // drone->land();
+    drone->navigate_wait({0, 0, 2, 0}, FRAME_LOCAL, 0.5);
+    // drone->sleep(3000);
+    circle(10, {0, 0, 2}, {0, 0, 2}, FRAME_LOCAL);
 
-    drone->navigate_wait({2, 0, 2, 0}, FRAME_LOCAL, 0.5);
+    // drone->navigate_wait({2, 0, 2, 0}, FRAME_LOCAL, 0.5);
 
-    drone->sleep(3000);
+    // drone->sleep(3000);
 
-    drone->navigate_wait({-2, 0, 2, 0}, FRAME_LOCAL, 0.5);
+    // drone->navigate_wait({-2, 0, 2, 0}, FRAME_LOCAL, 0.5);
 
-    drone->sleep(3000);
+    // drone->sleep(3000);
 
-    drone->navigate_wait({0, 0, 2, 1.57}, FRAME_LOCAL, 0.5);
-    // drone->set_position({0, 0, 2, 1.57}, FRAME_LOCAL);
-    drone->sleep(3000);
+    // drone->navigate_wait({0, 0, 2, 1.57}, FRAME_LOCAL, 0.5);
+    // // drone->set_position({0, 0, 2, 1.57}, FRAME_LOCAL);
+    // drone->sleep(3000);
     // drone->arm();
     // drone->takeoff(1.5, 0.5);
 
-    // drone->navigate({0, 0, 2, 0}, FRAME_VISION, 0.5);
-
+    drone->navigate({0, 0, 2, 0}, FRAME_VISION, 0.5);
+    drone->sleep(5000);
     drone->land();
-    // drone->sleep(5000);
-    // drone->disarm();
+    drone->sleep(5000);
+    drone->disarm();
 
     drone->stop();
     lwproto->stop();
